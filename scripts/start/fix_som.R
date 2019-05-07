17a6507fd566dbb12fbc9e8373851f5bdde2777d
Kristine Karstens <karstens@pik-potsdam.de>
2019-05-07 22:19:23 +0200

    ghg price on soils tests

--- scripts/start/fix_som.R
+++ scripts/start/fix_som.R
@@ -14,83 +14,34 @@ library(lucode)
 source("scripts/start_functions.R")
 source("config/default.cfg")
 
-### Runs without CC
-
-cfg$title   <- "BAU_static_som_nocc"
-cfg$gms$som <- "static_jan19"
-cfg$gms$c59_static_spatial_level <-  "cluster"
-start_run(cfg=cfg,codeCheck=TRUE)
-
-cfg$title   <- "BAU_dynamic_som_irr_nocc"
+cfg$title   <- "BAU_dynamic_som"
 cfg$gms$som <- "cellpool_aug16"
 cfg$gms$c59_irrigation_scenario  <- "on"
 start_run(cfg=cfg,codeCheck=TRUE)
 
-cfg$title   <- "BAU_dynamic_som_nocc"
-cfg$gms$som <- "cellpool_aug16"
-cfg$gms$c59_irrigation_scenario <- "off"
-start_run(cfg=cfg,codeCheck=TRUE)
+cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-Budg1300"
+cfg$gms$c60_2ndgen_biodem    <- "R2M41-SSP2-Budg1300"
 
-cfg$gms$c56_pollutant_prices <- "SSP2-26-SPA2-V15-REMIND-MAGPIE"
-cfg$gms$c60_2ndgen_biodem    <- "SSP2-26-SPA2"
-
-cfg$title   <- "SSP2-26-SPA2_static_som_nocc"
-cfg$gms$som <- "static_jan19"
-cfg$gms$c59_static_spatial_level <-  "cluster"
-start_run(cfg=cfg,codeCheck=TRUE)
-
-cfg$title   <- "SSP2-26-SPA2_dynamic_som_irr_nocc"
+cfg$title   <- "SSP2-26-SPA2_dynamic_som_none"
+cfg$gms$c56_emis_policy <- "none"
 cfg$gms$som <- "cellpool_aug16"
 cfg$gms$c59_irrigation_scenario  <- "on"
 start_run(cfg=cfg,codeCheck=TRUE)
 
-cfg$title   <- "SSP2-26-SPA2_dynamic_som_nocc"
-cfg$gms$som <- "cellpool_aug16"
-cfg$gms$c59_irrigation_scenario <- "off"
-start_run(cfg=cfg,codeCheck=TRUE)
-
-### Runs with CC
-
-#CC
-cfg$gms$c14_yields_scenario   <- "cc"
-cfg$gms$c42_watdem_scenario   <- "cc"
-cfg$gms$c43_watavail_scenario <- "cc"
-cfg$gms$c52_carbon_scenario   <- "cc"
-cfg$gms$c59_som_scenario      <- "cc" 
-
-cfg$gms$c56_pollutant_prices <- "SSP2-Ref-SPA0-V15-REMIND-MAGPIE"
-cfg$gms$c60_2ndgen_biodem    <- "SSP2-Ref-SPA0"
-
-cfg$title   <- "BAU_static_som_cc"
-cfg$gms$som <- "static_jan19"
-cfg$gms$c59_static_spatial_level <-  "cluster"
-start_run(cfg=cfg,codeCheck=TRUE)
-
-cfg$title   <- "BAU_dynamic_som_irr_nocc"
+cfg$title   <- "SSP2-26-SPA2_dynamic_som_all"
+cfg$gms$c56_emis_policy <- "all"
 cfg$gms$som <- "cellpool_aug16"
 cfg$gms$c59_irrigation_scenario  <- "on"
 start_run(cfg=cfg,codeCheck=TRUE)
 
-cfg$title   <- "BAU_dynamic_som_cc"
-cfg$gms$som <- "cellpool_aug16"
-cfg$gms$c59_irrigation_scenario <- "off"
-start_run(cfg=cfg,codeCheck=TRUE)
-
-cfg$gms$c56_pollutant_prices <- "SSP2-26-SPA2-V15-REMIND-MAGPIE"
-cfg$gms$c60_2ndgen_biodem    <- "SSP2-26-SPA2"
-
-cfg$title   <- "SSP2-26-SPA2_static_som_cc"
-cfg$gms$som <- "static_jan19"
-cfg$gms$c59_static_spatial_level <-  "cluster"
-start_run(cfg=cfg,codeCheck=TRUE)
-
-cfg$title   <- "SSP2-26-SPA2_dynamic_som_irr_cc"
+cfg$title   <- "SSP2-26-SPA2_dynamic_som_spp"
+cfg$gms$c56_emis_policy <- "spp"
 cfg$gms$som <- "cellpool_aug16"
 cfg$gms$c59_irrigation_scenario  <- "on"
 start_run(cfg=cfg,codeCheck=TRUE)
 
-cfg$title   <- "SSP2-26-SPA2_dynamic_som_cc"
+cfg$title   <- "SSP2-26-SPA2_dynamic_som_soiloff"
+cfg$gms$c56_emis_policy <- "soiloff"
 cfg$gms$som <- "cellpool_aug16"
-cfg$gms$c59_irrigation_scenario <- "off"
+cfg$gms$c59_irrigation_scenario  <- "on"
 start_run(cfg=cfg,codeCheck=TRUE)
-
